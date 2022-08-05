124c17dfe798b477d4371e6e4fc163bbcf4a74f5
Jegor <jegor.pakhutko@gmail.com>
2022-08-05 20:05:14 +0300

    minor fix

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -25,7 +25,7 @@ public class Main {
         char[] Arr = new char [5];
         for (int i = 0; i < 5; i++) {
             Arr[i] = ToSolve.charAt(i);
-            System.out.printf("\n%c", Arr[i]);
+//            System.out.printf("\n%c", Arr[i]);
         }
 
         if (Arr[1] == '=') {
